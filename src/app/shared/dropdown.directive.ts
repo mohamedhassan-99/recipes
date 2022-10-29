0c3611b9d1f4c77fe712f8451c7efed2675df350
Mohamed Hassan <92488204+mohamedhassan-99@users.noreply.github.com>
2022-10-29 20:55:32 +0200

    Fix the dropdown issue

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,26 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('click') toggleOpen(): void {
+    this.isOpen = !this.isOpen;
+    let part = this.elRef.nativeElement.querySelector('.dropdown-menu');
+    if (this.isOpen) {
+      this.renderer.addClass(part, 'show');
+    } else {
+      this.renderer.removeClass(part, 'show');
+    }
+  }
+}
