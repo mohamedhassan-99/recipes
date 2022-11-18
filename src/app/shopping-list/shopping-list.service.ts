2b69a53bee86626a09c5f0b04c153bbe726baa36
mohammed hassan <mohammed.hassan@yaquotitech.com>
2022-11-18 19:23:32 +0200

    Removing Event Emitters and replace it with Subjects

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingService{
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 10),
@@ -14,12 +14,12 @@ export class ShoppingService{
 
   addIngredient(ingredient: Ingredient){
    this.ingredients.push(ingredient);
-   this.ingredientsChanged.emit(this.ingredients.slice());
+   this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
 
   }
 }
