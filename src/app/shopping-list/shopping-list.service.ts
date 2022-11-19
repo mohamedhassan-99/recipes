c9e8efa10d9a52d962a3bd2f5d7936edbb4f5f51
mohammed hassan <mohammed.hassan@yaquotitech.com>
2022-11-19 17:05:42 +0200

    update service with some methods like update, get and delete

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -2,7 +2,11 @@ import { Subject } from 'rxjs';
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingService{
+  
   ingredientsChanged = new Subject<Ingredient[]>();
+
+  startedEditing = new Subject<number>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 10),
@@ -12,6 +16,19 @@ export class ShoppingService{
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
+  deleteIngredient(index : number){
+    this.ingredients.splice(index,1);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+  updateIngredient(index : number, newIngredient: Ingredient){
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+   }
+ 
   addIngredient(ingredient: Ingredient){
    this.ingredients.push(ingredient);
    this.ingredientsChanged.next(this.ingredients.slice());
