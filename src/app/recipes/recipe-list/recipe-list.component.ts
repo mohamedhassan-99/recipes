091b3971940aabd52af9710d091d7bf6ab69b84b
mohammed hassan <mohammed.hassan@yaquotitech.com>
2022-11-27 14:36:31 +0200

    subcripe to the recipes subject

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,25 @@
 import { RecipeService } from './../recipe.services';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit , OnDestroy{
   recipes!: Recipe[];
-
-  constructor(private recipeService: RecipeService) { }
-
-  ngOnInit() {
-    this.recipes = this.recipeService.getRecipes();
+  subscription !: Subscription;
+  constructor(private recipeService: RecipeService) {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
+  ngOnInit() {
+    this.subscription = this.recipeService.recipeChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
